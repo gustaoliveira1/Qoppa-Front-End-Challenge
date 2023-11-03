49a2e5dffcce2fc56e0b56f5012c7ecdc75bd00d
gustaoliveira <gusta01.oliveira@gmail.com>
2023-11-03 15:08:43 -0300

    feat: adding media queries for a responsive layout

--- web/src/pages/UserRegistration/index.tsx
+++ web/src/pages/UserRegistration/index.tsx
@@ -1,5 +1,7 @@
 import "./style.css";
 
+import abstractBackground from "../../assets/background.jpg";
+
 import { UserRegistrationForm } from "./components/UserRegistrationForm/"
 
 export function UserRegistration() {
@@ -8,6 +10,11 @@ export function UserRegistration() {
       id="user-registration"
       className="display-flex align-center wrapper"
     >
+
+      {/* left section */}
+      <img src={ abstractBackground } alt="an abstract blue background" />
+
+      {/* right section */}
       <UserRegistrationForm />
     </main>
   );
